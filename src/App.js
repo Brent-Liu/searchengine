484a4250565d72fc6f42b8b5608f62db08b0d1e7
Brent <lcncufe@gmail.com>
2022-07-12 15:38:55 -0700

    callback handler

--- src/App.js
+++ src/App.js
@@ -20,11 +20,17 @@ const App = () => {
       objectID: 1,
     },
   ]
+
+  const handleSearch = (event) => {
+    console.log(event.target.value)
+  }
+
+
   return (
     <div>
       <h1> My Hacker Stories </h1>
 
-      <Search />
+      <Search onSearch={handleSearch} />
 
       <hr />
 
@@ -34,11 +40,13 @@ const App = () => {
   )
 }
 
-const Search = () => {
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState('')
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value)
+
+    props.onSearch(event)
   }
   return (
     <div>
