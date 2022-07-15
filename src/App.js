1e63d96fbb48c81848312ddf1688999f49d8e860
Brent <lcncufe@gmail.com>
2022-07-15 14:24:45 -0700

    Fetching data from remote API

--- src/App.js
+++ src/App.js
@@ -1,31 +1,6 @@
 import * as React from 'react'
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-]
-
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  )
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query='
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -45,27 +20,27 @@ const storiesReducer = (state, action) => {
       return {
         ...state,
         isLoading: true,
-        isError: false,
+        isError: false
       }
     case 'STORIES_FETCH_SUCCESS':
       return {
         ...state,
         isLoading: false,
         isError: false,
-        data: action.payload,
+        data: action.payload
       }
     case 'STORIES_FETCH_FAILURE':
       return {
         ...state,
         isLoading: false,
-        isError: true,
+        isError: true
       }
     case 'REMOVE_STORY':
       return {
         ...state,
         data: state.data.filter(
           (story) => action.payload.objectID !== story.objectID
-        ),
+        )
       }
     default:
       throw new Error()
@@ -86,11 +61,12 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
+          payload: result.hits
         })
       })
       .catch(() =>
@@ -101,7 +77,7 @@ const App = () => {
   const handleRemoveStory = (item) => {
     dispatchStories({
       type: 'REMOVE_STORY',
-      payload: item,
+      payload: item
     })
   }
 
@@ -148,7 +124,7 @@ const InputWithLabel = ({
   type = 'text',
   onInputChange,
   isFocused,
-  children,
+  children
 }) => {
   const inputRef = React.useRef()
 
